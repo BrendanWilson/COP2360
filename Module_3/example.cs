aaacacc5cd5fc9ca4a9d262bf1f9a49a80f0aa01
Brendan Wilson <brendan.wilson@my.palmbeachstate.edu>
2024-06-02 12:32:09 -0400

    Updateing repo to include VSCode setting and sln

--- Module_3/example.cs
+++ Module_3/example.cs
@@ -1,15 +1,15 @@
+var o = new Octopus ("Jack", 5);
+Console.WriteLine($"Octopus {o.Name} has {o.Legs} legs and {o.eyes} eyes");
+
 class Octopus
 {
   public readonly string Name;
   public int Age;
-  public readonly int Legs = 8, eyes = 1;
+  public readonly int Legs = 8, eyes = 2;
   
   public Octopus (string name, int age)
   {
     Name = name;
-    Age = age
+    Age = age;
   }
 }
-
-var o = new Octopus ("Jack", 5);
-Console.WriteLine($"Octopus {o.Name} has {o.Legs} legs and {o.eyes} eyes");
