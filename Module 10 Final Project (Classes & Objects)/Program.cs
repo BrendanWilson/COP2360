1e7bf4c7447beb71059543918cb73b54ce6805d4
Juliette Viera <vierajm@my.palmbeachstate.edu>
2024-06-22 10:28:45 -0400

    Juliette Getting started with the Contractor Class

--- Module 10 Final Project (Classes & Objects)/Program.cs
+++ Module 10 Final Project (Classes & Objects)/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Part 1: Contractor Class
+public class Contractor
+{
+    private string ContractorName;
+    private string ContractorNumber;
+    private DateTime ContractorStartDate;
+
+    public string contractorname
+    {
+        get { return ContractorName; }
+        set { ContractorName = value; }
+
+    }
+
+    public string contractornam
+    {
+        get { return ContractorNumber; }
+        set { ContractorNumber = value; }
+
+    }
+
+    public DateTime contractorstartdate
+    {
+        get { return ContractorStartDate; }
+        set { ContractorStartDate = value; }
+    }
+
+}
+
+
+// Part 2: The Derived Subcontractor Class
+
+
+
+
+
+//Part 3: The Subcontractor Program
