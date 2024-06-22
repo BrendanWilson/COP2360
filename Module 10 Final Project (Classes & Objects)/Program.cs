85c838cb0c35e28f9feaab7b9bde8e11ec55e4d5
Juliette Viera <vierajm@my.palmbeachstate.edu>
2024-06-22 11:21:31 -0400

    Juliette 11:21 update to code

--- Module 10 Final Project (Classes & Objects)/Program.cs
+++ Module 10 Final Project (Classes & Objects)/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,10 +8,13 @@ using System.Threading.Tasks;
 // Part 1: Contractor Class
 public class Contractor
 {
+    // Control access to properties.
     private string ContractorName;
     private string ContractorNumber;
     private DateTime ContractorStartDate;
 
+    // Setting ContractorName, ContractorNumber and
+    // ContractorStartDate to public.
     public string contractorname
     {
         get { return ContractorName; }
@@ -18,7 +22,7 @@ public class Contractor
 
     }
 
-    public string contractornam
+    public string contractornumber
     {
         get { return ContractorNumber; }
         set { ContractorNumber = value; }
@@ -31,10 +35,26 @@ public class Contractor
         set { ContractorStartDate = value; }
     }
 
+
+// Constructor of the Contractor class to give name, number
+// and start date.
+    public Contractor(string name, string number, DateTime startDate)
+    {
+        contractorname = name;
+        contractornumber = number;
+        contractorstartdate = startDate;
+
+    }
 }
 
+// Part 2: The Derived Subcontractor Class using inheritance
 
-// Part 2: The Derived Subcontractor Class
+public class Subcontractor : Contractor
+{
+    public static int Shift;
+    public static double HourlyPayRate;
+
+
 
 
 
